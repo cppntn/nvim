e896b6e93bc862870e004483641fe361c83685a6
Antonio Cappiello <antoniocappiello93@icloud.com>
2024-03-06 16:21:57 +0100

    add keymap for LSP code action

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -48,7 +48,7 @@ return {
           }
         }
       })
-
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
     end
