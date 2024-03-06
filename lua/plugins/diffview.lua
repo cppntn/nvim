a4231022fe5842d8be271d783565962503443fb6
Antonio Cappiello <antoniocappiello93@icloud.com>
2024-03-06 10:54:01 +0100

    keymaps for diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -2,12 +2,8 @@ return {
   "sindrets/diffview.nvim",
   lazy = false,
   config = function()
-    vim.keymap.set('n', '<leader>gd', function()
-      if next(require("diffview.lib").views) == nil then
-        vim.cmd("DiffviewOpen")
-      else
-        vim.cmd("DiffviewClose")
-      end
-    end, { silent = true })
+    -- Key mapping to open Diffview
+    vim.keymap.set('n', '<leader>gd', ':DiffviewOpen <CR>', { silent = true })
+    vim.keymap.set('n', '<leader>gdc', ':DiffviewClose <CR>', { silent = true })
   end
 }
