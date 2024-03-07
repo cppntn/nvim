00e4e2292af461887d83be39a4f82f29876237d5
Antonio Cappiello <antoniocappiello93@icloud.com>
2024-03-07 12:54:58 +0100

    add keymaps for ctrl-k and ctrl-d

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,10 +2,8 @@
 vim.keymap.set('i', '<C-a>', '<ESC>^i', { desc = "Beginning of line" })
 vim.keymap.set('i', '<C-b>', '<ESC>^i', { desc = 'Beginning of line' })
 vim.keymap.set('i', '<C-e>', '<End>', { desc = 'End of line' })
-vim.keymap.set('i', '<C-h>', '<Left>', { desc = 'Move left' })
-vim.keymap.set('i', '<C-l>', '<Right>', { desc = 'Move right' })
-vim.keymap.set('i', '<C-j>', '<Down>', { desc = 'Move down' })
-vim.keymap.set('i', '<C-k>', '<Up>', { desc = 'Move up' })
+vim.keymap.set('i', '<C-d>', '<Del>', { desc = "Backward delete" })
+vim.keymap.set('i', '<C-k>', '<C-o>D', { desc = "Delete to end of line" })
 
 -- Normal mode mappings
 vim.keymap.set('n', ';', ':', { nowait = true })
