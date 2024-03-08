5bb80aa2e415f06f90120c0eefce6daeb5fbbebc
Antonio Cappiello <antoniocappiello93@icloud.com>
2024-03-08 11:05:18 +0100

    Custom function to open git_commits under DiffviewOpen

--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -4,9 +4,32 @@ return {
     tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
+      local actions = require('telescope.actions')
+      local action_state = require('telescope.actions.state')
+      local telescope = require('telescope')
+
+      telescope.setup({})
+
+      -- Custom function to open git_commits under DiffviewOpen
+      local function git_commits_with_diffview()
+        require('telescope.builtin').git_commits({
+          attach_mappings = function(prompt_bufnr, map)
+            map('i', '<CR>', function()
+              local selection = action_state.get_selected_entry(prompt_bufnr)
+              actions.close(prompt_bufnr)
+              vim.cmd("DiffviewOpen " .. selection.value .. "^.." .. selection.value)
+            end)
+            return true
+          end,
+        })
+      end
+
+      -- Assign the custom function to a command for easy access
+      vim.api.nvim_create_user_command('GitCommitsDiffview', git_commits_with_diffview, {})
+      vim.keymap.set('n', '<leader>cm', ':GitCommitsDiffview <CR>', {})
+      -- Keymaps
       vim.keymap.set('n', '<leader>ff', ':Telescope find_files follow=true no_ignore=true hidden=true <CR>', {})
       vim.keymap.set('n', '<leader>fw', ':Telescope live_grep <CR>', {})
-      vim.keymap.set('n', '<leader>cm', ':Telescope git_commits <CR>', {})
     end
   },
   {
