a6b28870c3e19f3a6e58e24ccf359ec006f6a812
Antonio Cappiello <antoniocappiello93@icloud.com>
2024-03-08 15:26:44 +0100

    add todo-comments search

--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -53,5 +53,14 @@ return {
       })
       require("telescope").load_extension("ui-select")
     end
+  },
+  -- todo-comments
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("todo-comments").setup()
+      vim.keymap.set('n', '<leader>ft', ':TodoTelescope <CR>', {})
+    end
   }
 }
